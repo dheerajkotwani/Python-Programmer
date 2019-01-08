6186420178e4038f0be112160d5c9720ccc9ca73
Dheeraj Kotwani <44086235+dheerajkotwani@users.noreply.github.com>
2019-01-08 10:49:58 +0530

    editor1.py
    Duck Typing in python

---
+++ editor1.py
@@ -0,0 +1,26 @@
+class pyCharm:
+
+    def editor(self):
+        print("For pyCharm")
+        print("Compile")
+        print("Run")
+
+class MyEditor:
+
+    def editor(self):
+        print("For MyEditor")
+        print("Spell Check")
+        print("Errors")
+        print("Compile")
+        print("Run")
+
+class Laptop:
+
+    def code(self,ide):
+        ide.editor()
+
+
+ide=pyCharm()
+
+lap1=Laptop()
+lap1.code(ide)
